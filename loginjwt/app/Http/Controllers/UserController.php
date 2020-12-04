1321af595eb6cf27b109355d7ecbf94723f87228
NesSan974 <tintin974run.qc@gmail.com>
2020-12-04 10:35:22 +0100

    log after 3 attempts

---
+++ loginjwt/app/Http/Controllers/UserController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+use JWTAuth;
+use App\Models\User;
+use phpDocumentor\Reflection\Location;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+session_start();
+
+class UserController extends Controller
+{
+
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+
+        $expireSessionTime = 65;
+        if (!isset($_SESSION['CREATED'])) {
+            $_SESSION['CREATED'] = time();
+          }else if (time() - $_SESSION['CREATED'] > $expireSessionTime){
+            session_unset(); 
+            session_destroy();
+          }
+          
+          if (!isset($_SESSION['count'])) {
+            $_SESSION['count'] = 1;
+          }
+          
+        if ($_SESSION['count'] >= 3) {
+            $i = $expireSessionTime - (time() - $_SESSION['CREATED'] );
+            error_log("tentative de connection superieur a 3, avec l'ip : ".$_SERVER['REMOTE_ADDR']."\n", 3, '/home/quentin/loginjwt/storage/logs/tentativeLogin.log');
+            return "too much attempt, revenez dans :".round ($i/60) . 'minute(s) ou '.$i.'s' ;
+        }
+        
+
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                $_SESSION['count']++;
+                return response()->json(['error' => 'invalid credential', 'token' => $token], 401);;
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        $_SESSION['count'] = 0;
+        return response()->json(compact('token'));
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user', 'token'), 201);
+    }
+
+    public function getAuthenticatedUser()
+    {
+        try {
+
+            if (!$user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+
+            return response()->json(['token_expired'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+
+            return response()->json(['token_invalid'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+
+            return response()->json(['token_absent'], $e->getStatusCode());
+        }
+
+        return response()->json(compact('user'));
+    }
+}
---
+++ loginjwt/resources/views/welcome.blade.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Laravel</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+
+  <style>
+    body {
+      font-family: 'Nunito';
+    }
+  </style>
+</head>
+
+<body class="antialiased">
+  <form method="POST" action="/api/login">
+    <div class="form-group">
+      <label for="Email">Email address</label>
+      <input type="email" name="email" class="form-control" aria-describedby="emailHelp">
+      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="form-group form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <!-- <input name="count" type="hidden" value="<?php //echo $_SESSION['count'] ?>"> -->
+    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+  </form>
+
+
+
+</body>
+
+</html>
